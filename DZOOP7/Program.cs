7b85843bae7206abd088f2c9940415c831569dd1
ZewsSD <76225337+ZewsSD@users.noreply.github.com>
2021-05-13 19:37:39 +0300

    Update Program.cs

--- DZOOP7/Program.cs
+++ DZOOP7/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,35 +8,75 @@ namespace DZOOP7
 {
     class Client
     {
+        public string[] FoodBasket { get; private set; }
+        public static Random Rnd { get; private set; }
+
         private int _money;
-        private string[] _products;
 
         public Client()
         {
             _money = 5000;
+            FoodBasket = new string[Rnd.Next(5, 10 + 1)];
         }
 
-        public void GiveAwayBasket(Magazin magazin)
+        static Client()
         {
-
+            Rnd = new Random();
         }
 
-        public void GetTotalPrice(Magazin magazin)
+        public void FillFoodBasket(Magazin magazin)
         {
+            for (int i = 0; i < magazin.Products.Length; i++)
+            {
+                for (int j = 0; j < FoodBasket.Length; j++)
+                {
+                    FoodBasket[j] = magazin.Products[i].Name;
+                }
+            }
+        }
 
+        public bool Pay(Magazin magazin, bool isPay)
+        {
+            if (_money >= magazin.TotalPrice)
+            {
+                _money -= magazin.TotalPrice;
+            }
+            else
+            {
+
+            }
+
+            return isPay;
         }
     }
 
     class Magazin
     {
+        public Product[] Products { get; private set; }
+        public int TotalPrice { get; private set; }
+
         public Magazin()
         {
-            Product[] products = { new Product("огурцец", 15), new Product("дошик", 45), new Product("помидор", 20), new Product("шоколадка", 100), new Product("молоко", 25) };
+            Products = new Product[5];
+            Products[0] = new Product("огурцец", 15);
+            Products[1] = new Product("дошик", 45);
+            Products[2] = new Product("помидор", 20);
+            Products[3] = new Product("шоколадка", 100);
+            Products[4] = new Product("молоко", 25);
         }
 
         public void CountTotalPrice(Client client)
         {
-
+            for (int i = 0; i < client.FoodBasket.Length; i++)
+            {
+                for (int j = 0; j < Products.Length; j++)
+                {
+                    if (client.FoodBasket[i] == Products[j].Name)
+                    {
+                        TotalPrice += Products[j].Price;
+                    }
+                }
+            }
         }
     }
 
@@ -56,6 +96,29 @@ namespace DZOOP7
     {
         static void Main(string[] args)
         {
+            Client[] clients = new Client[3];
+            clients[0] = new Client();
+            clients[1] = new Client();
+            clients[2] = new Client();
+
+            Magazin magazin = new Magazin();
+
+            bool isPay = true;
+
+            for (int i = 0; i < clients.Length; i++)
+            {
+                clients[i].FillFoodBasket(magazin);
+                magazin.CountTotalPrice(clients[i]);
+
+                isPay = clients[i].Pay(magazin, isPay);
+
+                if (isPay == true)
+                {
+                    Console.WriteLine("Покупка прошла успешно");
+                }
+            }
+
+            Console.ReadKey();
         }
     }
 }
